2ddff5ff2ba61912248ab4a556b99f12b31e8d3c
VladkoFateich <92536769+VladkoFateich@users.noreply.github.com>
2024-06-21 19:45:42 +0500

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,31 +1,44 @@
-let wrapper = document.querySelector("#wrapper"),
-  main = document.querySelector("#main"),
-  arrBlocks = [];
-for (let i = 1; i <= 76; i++) {
-  let block = document.createElement("article"),
-    num = document.createElement("p");
+// let wrapper = document.querySelector("#wrapper"),
+//   main = document.querySelector("#main"),
+//   arrBlocks = [];
+// for (let i = 1; i <= 76; i++) {
+//   let block = document.createElement("article"),
+//     num = document.createElement("p");
 
-  block.classList.add("article");
-  num.textContent = i;
+//   block.classList.add("article");
+//   num.textContent = i;
 
-  main.append(block);
-  block.append(num);
-  arrBlocks.push(block);
+//   main.append(block);
+//   block.append(num);
+//   arrBlocks.push(block);
 
-  for (let j = 0; j <= 5; j++) {
-    let txt = document.createElement("hr");
-    block.append(txt);
-  }
+//   for (let j = 0; j <= 5; j++) {
+//     let txt = document.createElement("hr");
+//     block.append(txt);
+//   }
+// }
+// for (let k = 1; k <= 33; k++) {
+//   let res;
+//   res = arrBlocks[Math.floor(Math.random() * arrBlocks.length)];
+//   for(let item of arrBlocks) {
+//     if(res === item) {
+//         let img = document.createElement('div')
+//         img.classList.add('img')
+//         img.textContent = 'IMAGE'
+//         item.prepend(img)
+//     }
+//   }
+// }
+
+
+let images = document.querySelectorAll('.image'),
+articleTitles = document.querySelectorAll('.articleTitle'),
+imgLink = 'https://wp-s.ru/wallpapers/9/19/508322161627456/foto-zelenoj-doliny-na-fone-golubogo-neba.jpg'
+
+for(let image of images) {
+    image.src = imgLink
 }
-for (let k = 1; k <= 33; k++) {
-  let res;
-  res = arrBlocks[Math.floor(Math.random() * arrBlocks.length)];
-  for(let item of arrBlocks) {
-    if(res === item) {
-        let img = document.createElement('div')
-        img.classList.add('img')
-        img.textContent = 'IMAGE'
-        item.prepend(img)
-    }
-  }
+for(let articleTitle of articleTitles) {
+    articleTitle.textContent = 'Title'
 }
+
